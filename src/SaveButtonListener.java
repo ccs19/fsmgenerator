3b242fa9922afe05558647b4a95835c6d643d538
ccs19@students.uwf.edu <ccs19@students.uwf.edu>
2015-03-03 21:25:14 -0600

    Successfully implemented saving

--- src/SaveButtonListener.java
+++ src/SaveButtonListener.java
@@ -1,6 +1,9 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 
 import static java.lang.Integer.*;
@@ -38,10 +41,11 @@ public class SaveButtonListener implements ActionListener {
             {
                 dialogMessage += unsafeSaveReasons.get(i) + "\n";
             }
+            //Show dialog with input errors found
             JOptionPane.showMessageDialog(null, dialogMessage, "Cannot save", JOptionPane.OK_OPTION);
         }
         else {
-            JFileChooser jFileChooser = new JFileChooser();
+            saveAutomaton();
         }
     }
 
@@ -55,7 +59,7 @@ public class SaveButtonListener implements ActionListener {
 
     private void checkNumStates()
     {
-        String numStates = listenPanel.getNumStates();
+        numStates = listenPanel.getNumStates();
         try
         {
             Integer.parseInt(numStates);
@@ -68,17 +72,17 @@ public class SaveButtonListener implements ActionListener {
 
     private void checkAlphabet()
     {
-
+        alphabet = listenPanel.getAlphabet();
     }
 
     private void checkAccepStates()
     {
-
+        acceptStates = listenPanel.getAcceptStates();
     }
 
     private void checkStateTransitions()
     {
-
+        stateTransitions = listenPanel.getTransitions();
     }
 
     private void checkStartState()
@@ -91,6 +95,31 @@ public class SaveButtonListener implements ActionListener {
         catch (NumberFormatException e)
         {
             unsafeSaveReasons.add("Invalid entry in Start State");
+        }
+    }
+
+    private void saveAutomaton()
+    {
+        JFileChooser jFileChooser = new JFileChooser();
+        int returnVal = jFileChooser.showSaveDialog(listenPanel);
+
+        if(returnVal == JFileChooser.APPROVE_OPTION)
+        {
+            File file = jFileChooser.getSelectedFile();
+
+            try {
+                BufferedWriter fileWriter = new BufferedWriter(new FileWriter(file));
+                fileWriter.write(numStates + "\n" +
+                                 alphabet + "\n" +
+                                 stateTransitions + "\n" +
+                                 startState + "\n" +
+                                 acceptStates);
+                fileWriter.close();
+            }
+            catch (Exception e)
+            {
+                JOptionPane.showMessageDialog(null, "Failed to save. Cannot write to disk.", "Fatal Error", JOptionPane.OK_OPTION);
+            }
         }
     }
 }
