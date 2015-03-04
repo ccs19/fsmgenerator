062a68cbd5939e0058b47cbfed418605d6d1bba7
ccs19@students.uwf.edu <ccs19@students.uwf.edu>
2015-03-03 20:16:02 -0600

    GUI Revised further.

--- src/FsmFrame.java
+++ src/FsmFrame.java
@@ -15,7 +15,7 @@ public class FsmFrame extends JFrame {
         this.getContentPane().add(new FsmPanel());
 
 
-        this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.pack();
         this.setVisible(true);
     }
--- src/FsmPanel.java
+++ src/FsmPanel.java
@@ -18,43 +18,120 @@ public class FsmPanel extends JPanel
     final private int JTF_STARTSTATE = 5;
     final private int JTF_ACCEPTSTATES = 10;
 
+
+    //Text field label strings
     final private String numStatesString = "Number of states";
     final private String startStateString = "Start State";
-    final private String acceptStatesString = "Accept States";
-    final private String alphabetString = "Alphabet";
-    final private String transitionsString = "State Transistions";
+    final private String acceptStatesString = "Accept States (Seperated by commas)";
+    final private String alphabetString = "Alphabet (Seperated by commas)";
+    final private String[] transitionsString = {"State Transistions (Seperate by commas)" , "Format: (current:dest:input)"};
     final private String saveString = "Save";
     final private String exitString = "Exit";
     final private String exitConfirmMessage = "Are you sure you want to exit?\nUnsaved changes will be lost.";
 
+
+    //GridBagConstraints padding
+    Insets labelInset = new Insets(10,10,0,10);
+    Insets textFieldInset = new Insets(0,10,10,10);
+
+
+    //JTextFields
     private JTextField numStates,
             startState,
             acceptStates,
             alphabet,
             transitions;
+    ////////////////////
 
     FsmPanel()
     {
         super(new GridBagLayout());
-
-        Insets labelInset = new Insets(10,10,0,10);
-        Insets textFieldInset = new Insets(0,10,10,10);
         GridBagConstraints gbc = new GridBagConstraints();
-        gbc.fill = GridBagConstraints.BOTH;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
 
+
+        /**Top row**/
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 3;
+        this.add(topRow(), gbc);
+
+        /**Alphabet**/
+        JLabel alphabetLabel = new JLabel(alphabetString);
+        gbc.gridx=0;
+        gbc.gridy=1;
+        gbc.insets = labelInset;
+        gbc.gridwidth = 1;
+        this.add(alphabetLabel, gbc);
+
+        alphabet = new JTextField(JTF_ALPHABET);
+        gbc.gridx=0;
+        gbc.gridy=2;
+        gbc.insets = textFieldInset;
+        gbc.gridwidth = 3;
+        this.add(alphabet, gbc);
+        /**********************************/
+
+
+
+        /**State Transisitions**/
+        JLabel transitionsLabel = new JLabel(transitionsString[0]);
+        gbc.gridx=0;
+        gbc.gridy=3;
+        gbc.insets = labelInset;
+        gbc.gridwidth = 1;
+        this.add(transitionsLabel, gbc);
+
+        JLabel transitionsLabel2 = new JLabel(transitionsString[1]);
+        gbc.gridx=0;
+        gbc.gridy=4;
+        gbc.insets = new Insets(0,10,0,10);
+        gbc.gridwidth = 1;
+        this.add(transitionsLabel2,gbc);
+
+        transitions = new JTextField(JTF_TRANSITIONS);
+        gbc.gridx=0;
+        gbc.gridy=5;
+        gbc.insets = textFieldInset;
+        gbc.gridwidth = 3;
+        this.add(transitions, gbc);
+        /**********************************/
+
+
+        /**Buttons**/
+        gbc.gridx=1;
+        gbc.gridy=6;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.LAST_LINE_END;
+        this.add(addButtons(),gbc );
+
+
+        this.setVisible(true);
+    }
+
+
+
+
+
+
+    private JPanel topRow()
+    {
+        GridBagConstraints gbc = new GridBagConstraints();
+        JPanel topRow = new JPanel(new GridBagLayout());
+        gbc.fill = GridBagConstraints.HORIZONTAL;
 
         /**Number of states**/
         JLabel numStatesLabel = new JLabel(numStatesString);
         gbc.gridx=0;
         gbc.gridy=0;
         gbc.insets = labelInset;
-        this.add(numStatesLabel, gbc);
+        topRow.add(numStatesLabel, gbc);
 
         numStates = new JTextField(JTF_NUMSTATES);
         gbc.gridx=0;
         gbc.gridy=1;
         gbc.insets = textFieldInset;
-        this.add(numStates, gbc);
+        topRow.add(numStates, gbc);
         /***********************************/
 
 
@@ -63,13 +140,13 @@ public class FsmPanel extends JPanel
         gbc.gridx=1;
         gbc.gridy=0;
         gbc.insets = labelInset;
-        this.add(startStateLabel, gbc);
+        topRow.add(startStateLabel, gbc);
 
         startState = new JTextField(JTF_STARTSTATE);
         gbc.gridx=1;
         gbc.gridy=1;
         gbc.insets = textFieldInset;
-        this.add(startState, gbc);
+        topRow.add(startState, gbc);
         /**********************************/
 
         /**Accept states**/
@@ -77,60 +154,30 @@ public class FsmPanel extends JPanel
         gbc.gridx=2;
         gbc.gridy=0;
         gbc.insets = labelInset;
-        this.add(acceptStatesLabel, gbc);
+        topRow.add(acceptStatesLabel, gbc);
 
         acceptStates = new JTextField(JTF_ACCEPTSTATES);
         gbc.gridx=2;
         gbc.gridy=1;
         gbc.insets = textFieldInset;
-        this.add(acceptStates, gbc);
+        topRow.add(acceptStates, gbc);
         /**********************************/
-
-
-        /**Alphabet**/
-        JLabel alphabetLabel = new JLabel(alphabetString);
-        gbc.gridx=0;
-        gbc.gridy=2;
-        gbc.insets = labelInset;
-        this.add(alphabetLabel, gbc);
-
-        alphabet = new JTextField(JTF_ALPHABET);
-        gbc.gridx=0;
-        gbc.gridy=3;
-        gbc.insets = textFieldInset;
-        gbc.gridwidth=3;
-        this.add(alphabet, gbc);
-        /**********************************/
+        return topRow;
+    }
 
 
 
-        /**State Transisitions**/
-        JLabel transitionsLabel = new JLabel(transitionsString);
-        gbc.gridx=0;
-        gbc.gridy=4;
-        gbc.insets = labelInset;
-        gbc.gridwidth=1;
-        this.add(transitionsLabel, gbc);
 
-        transitions = new JTextField(JTF_TRANSITIONS);
-        gbc.gridx=0;
-        gbc.gridy=5;
-        gbc.insets = textFieldInset;
-        gbc.gridwidth=3;
-        this.add(transitions, gbc);
-        /**********************************/
-
+    private JPanel addButtons()
+    {
+        //JPanel to return
+        JPanel buttonsPanel = new JPanel(new GridLayout(1,3,10,0));
 
         /**Save Button**/
         JButton saveButton = new JButton(saveString);
         saveButton.addActionListener(new SaveButtonListener(this));
-        gbc.gridx=1;
-        gbc.gridy=6;
-        gbc.gridwidth = 1;
-        gbc.insets = new Insets(20,10,10,10);
-        this.add(saveButton, gbc);
+        buttonsPanel.add(saveButton);
         /**********************************/
-
 
         /**Exit Button**/
         JButton exitButton = new JButton(exitString);
@@ -144,12 +191,10 @@ public class FsmPanel extends JPanel
                 }
             }
         });
-        gbc.gridx=2;
-        gbc.gridy=6;
-        this.add(exitButton, gbc);
+        buttonsPanel.add(exitButton);
         /**********************************/
 
-        this.setVisible(true);
+        return buttonsPanel;
     }
 
 
